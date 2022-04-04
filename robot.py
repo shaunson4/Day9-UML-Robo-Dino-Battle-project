51ec04014ed1ccab123699d01b93c78fb2e36db5
shaunson4 <sgray4106@gmail.com>
2022-04-04 16:20:06 -0500

    Added the Weapon and Robot classes to the project

---
+++ robot.py
@@ -0,0 +1,6 @@
+from weapon import Weapon
+class Robot:
+    def __init__(self, name):
+        self.name = name
+        self.health = 100
+        self.active_weapon = Weapon('grenade launcher', 50)
